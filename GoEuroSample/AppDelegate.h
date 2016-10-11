6a738d9c23097132969cd12d895145c816bd1455
Ben Joe Mathews <joemathews444@gmail.com>
2016-10-11 22:28:22 +0530

    Initial Commit

---
+++ GoEuroSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoEuroSample
+//
+//  Created by Ben Joe Mathews on 11/10/16.
+//  Copyright © 2016 Ben Joe Mathews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoEuroSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoEuroSample
+//
+//  Created by Ben Joe Mathews on 11/10/16.
+//  Copyright © 2016 Ben Joe Mathews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
